1fe677cea433080ce3a703ed15a7356efc83df98
Shivam Saxena <shivamsaxena862006@gmail.com>
2026-05-28 23:54:34 +0530

    Modele Trainer

--- src/components/data_injection.py
+++ src/components/data_injection.py
@@ -2,11 +2,16 @@ import os
 import sys
 from src.logger import logging
 from src.exception import CustomException
+from src.utils import save_object
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -52,4 +57,6 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_path, test_path = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_path, test_path)
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+from sklearn.metrics import r2_score
 import src.logger as logging
 from src.exception import CustomException
 
@@ -16,4 +18,22 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         logging.error(f"Error occurred while saving object: {str(e)}")
+        raise CustomException(e,sys)
+    
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+
+            y_test_pred=model.predict(X_test)
+
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+        return report
+    except Exception as e:
+        logging.error(f"Error occurred while evaluating models: {str(e)}")
         raise CustomException(e,sys)
